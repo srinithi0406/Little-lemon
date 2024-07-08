939d3174d38f027da9b749a7fdb8588c3fcc5e28
srinithi0406 <itsmesrinithi@gmail.com>
2024-07-08 13:01:28 -0700

    components

---
+++ littleapp/src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from "react"
+function Nav(){
+    return(
+        <nav>
+            <img src="logo.jpg" alt="logo"/>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Menu</a></li>
+                <li><a href="">Reserve table</a></li>
+                <li><a href="">Order online</a></li>
+                <li><a href="">Login</a></li>
+            </ul>
+        </nav>
+
+    )
+}
+export default Nav
